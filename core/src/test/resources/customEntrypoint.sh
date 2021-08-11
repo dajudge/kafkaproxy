5c35a87a70f1e20ac6e49a28ac5872cb50c43c57
Alex Stockinger <mail@alexstockinger.de>
2021-08-11 16:11:45 +0200

    Update license header
    Fixes #34

--- core/src/test/resources/customEntrypoint.sh
+++ core/src/test/resources/customEntrypoint.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-# Copyright 2019-2020 The kafkaproxy developers (see CONTRIBUTORS)
+# Copyright 2019-2021 The kafkaproxy developers (see CONTRIBUTORS)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
